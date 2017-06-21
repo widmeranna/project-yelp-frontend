c08daaa2645a8034e9ede02dd1d1418e01efada2
widmeranna <widmeranna@gmail.com>
2017-06-21 11:46:00 +0200

    included from method extension of Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,27 +3,31 @@ import './index.css';
 import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 
-const Contact = () => (
-  <div className="Contact-form">
-    <h3>We would love to hear from you!</h3>
-    <TextField
-      className="Email-input"
-      type="email"
-      floatingLabelText="Your email:"
-    />
-    <br/>
-    <br/>
-    <TextField
-      className="Message-input"
-      floatingLabelText="Your message:"
-      multiLine={true}
-      style={{textAlign: 'left'}}
-      rows={3}
-      type="text"
-    />
-    <br/>
-    <FlatButton label="Send"/>
-  </div>
-)
+class Contact extends Component {
+  render(){
+    return(
+      <div className="Contact-form">
+        <h3>We would love to hear from you!</h3>
+        <TextField
+          className="Email-input"
+          type="email"
+          floatingLabelText="Your email:"
+        />
+        <br/>
+        <br/>
+        <TextField
+          className="Message-input"
+          floatingLabelText="Your message:"
+          multiLine={true}
+          style={{textAlign: 'left'}}
+          rows={3}
+          type="text"
+        />
+        <br/>
+        <FlatButton label="Send"/>
+      </div>
+    )
+  }
+}
 
 export default Contact;
