54c1702a28470b8cf73e0772f24a792fd3adbd49
widmeranna <widmeranna@gmail.com>
2017-06-21 09:34:50 +0200

    added basic contact page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+  render(){
+    return (
+      <div className="Contact-page">
+        <h2>We would love to hear from you!</h2>
+        <p>Contact us at team2@project.com</p>
+      </div>
+    )
+  }
+}
+
+export default Contact;
