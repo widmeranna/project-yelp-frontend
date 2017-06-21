fdb7c0f839a7ec1c2c232b5760d9340b8b6ad954
Satuk Kolan <34@satuk.ch>
2017-06-21 15:07:27 +0200

    wrapped the children component in a div

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -82,9 +82,6 @@ export default class Header extends Component {
             this.props.children
           }
         </div>
-        {
-          this.props.children
-        }
       </div>
     );
   }
