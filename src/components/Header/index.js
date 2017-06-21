f2f759903144955be0892aab981e3fe5d387efcc
Satuk Kolan <34@satuk.ch>
2017-06-21 15:02:58 +0200

    added a simple login and logout stateless components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,6 +15,23 @@ class Header extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <h2>Project Yelp - Team 2 - Frontend</h2>
+const Login = () => {
+  return(
+    <div>
+      <Link to="/sign-in"><FlatButton style={styles.buttonTextColor} label="Sign In" /></Link>
+      <Link to="/sign-up"><FlatButton style={styles.buttonTextColor} label="Sign Up" /></Link>
+    </div>
+  );
+}
+
+const Logged = () => {
+  return(
+    <div>
+      <Link to="/edit-profile"><FlatButton style={styles.buttonTextColor} label="Edit Profile" /></Link>
+      <Link to="/sign-out"><FlatButton style={styles.buttonTextColor} label="Sign Out" /></Link>
+    </div>
+  );
+};
         </div>
         {
           this.props.children
