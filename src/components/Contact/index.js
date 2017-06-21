2ff895214eda115aca8e864c89a4edc15d7789b8
widmeranna <widmeranna@gmail.com>
2017-06-21 11:34:39 +0200

    added input form with material-ui

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
+import './index.css';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
-class Contact extends Component {
-  render(){
-    return (
-      <div className="Contact-page">
-        <h2>We would love to hear from you!</h2>
-        <p>Contact us at team2@project.com</p>
-      </div>
-    )
-  }
-}
+const Contact = () => (
+  <div className="Contact-form">
+    <h3>We would love to hear from you!</h3>
+    <TextField
+      className="Email-input"
+      type="email"
+      floatingLabelText="Your email:"
+    />
+    <br/>
+    <br/>
+    <TextField
+      className="Message-input"
+      floatingLabelText="Your message:"
+      multiLine={true}
+      style={{textAlign: 'left'}}
+      rows={3}
+      type="text"
+    />
+    <br/>
+    <FlatButton label="Send"/>
+  </div>
+)
 
 export default Contact;
