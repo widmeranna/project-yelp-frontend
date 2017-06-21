43f1c56d560e7d88deed0e46aae935d5d0349f9a
Satuk Kolan <34@satuk.ch>
2017-06-21 15:53:09 +0200

    converted the component to stateles component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class About extends Component {
-  render(){
-    return(
-      <div className ="About-page">
-        <h1>Welcome to Yelpdemo!</h1>
-        <p>This is one of the sample appications for BaseRails, a platfrom that teaches people to build awesome custom websites with the Ruby on Rails programming language. Sign up and start learning today!</p>
-      </div>
-    );
-  }
+const About = () => {
+  return (
+    <div className ="About-page">
+      <h1>Welcome to Yelpdemo!</h1>
+      <p>This is one of the sample appications for BaseRails, a platfrom that teaches people to build awesome custom websites with the Ruby on Rails programming language. Sign up and start learning today!</p>
+    </div>
+  );
 }
 
 export default About;
