482dc001fe84f2e588fe9e09aa443157bf8db579
Satuk Kolan <34@satuk.ch>
2017-06-22 09:18:14 +0200

    refactored the functional component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const About = () => {
-  return (
-    <div className ="About-page">
-      <h1>Welcome to Yelpdemo!</h1>
-      <p>This is one of the sample appications for BaseRails, a platfrom that teaches people to build awesome custom websites with the Ruby on Rails programming language. Sign up and start learning today!</p>
-    </div>
-  );
-}
+const About = () => (
+  <div className ="About-page">
+    <h1>Welcome to Yelpdemo!</h1>
+    <p>This is one of the sample appications for BaseRails, a platfrom that teaches people to build awesome custom websites with the Ruby on Rails programming language. Sign up and start learning today!</p>
+  </div>
+);
 
 export default About;
