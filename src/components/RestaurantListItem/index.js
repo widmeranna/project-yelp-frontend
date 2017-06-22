ca74cc6f629a7324b8bab3d54d02cbe359afcc7e
widmeranna <widmeranna@gmail.com>
2017-06-22 18:09:41 +0200

    added path to restaurant info if click on restaurant name

--- src/components/RestaurantListItem/index.js
+++ src/components/RestaurantListItem/index.js
@@ -3,12 +3,13 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import { Link } from 'react-router-dom';
 
 const RestaurantListItem = (props) => {
 	const {restaurant} = props;
 	return(
 		<TableRow>
-			<TableRowColumn>{restaurant.name}</TableRowColumn>
+			<TableRowColumn><Link to={'restaurants/' + restaurant.id}>{restaurant.name}</Link></TableRowColumn>
 			<TableRowColumn>{restaurant.address}</TableRowColumn>
 			<TableRowColumn>{restaurant.phone}</TableRowColumn>
 			<TableRowColumn><a href={"http://"+restaurant.website} target="_blank" >{restaurant.website}</a></TableRowColumn>
