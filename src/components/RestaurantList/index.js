d71a2ca6c02d6aed7216c79a6b897fb4ab81751e
Satuk Kolan <34@satuk.ch>
2017-06-22 15:20:38 +0200

    fixed restaurant list

--- src/components/RestaurantList/index.js
+++ src/components/RestaurantList/index.js
@@ -8,25 +8,39 @@ import {
   TableRow,
 } from 'material-ui/Table';
 import RestaurantListItem from '../RestaurantListItem';
+import './index.css';
+import city from './city.jpg'; //better to use png or svg
 
 const RestaurantList = ({restaurants}) => (
-  <Table>
-    <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-      <TableRow>
-        <TableHeaderColumn>Name</TableHeaderColumn>
-          <TableHeaderColumn>Address</TableHeaderColumn>
-          <TableHeaderColumn>Phone</TableHeaderColumn>
-          <TableHeaderColumn>Website</TableHeaderColumn>
-        </TableRow>
-    </TableHeader>
-    <TableBody displayRowCheckbox={false}>
-      {
-        restaurants.map(rest =>
-          <RestaurantListItem key={rest.id} restaurant={rest} />
-        )
-      }
-    </TableBody>
-  </Table>
+  <div className="Home-List">
+    <div className="descr-restaurant-list">
+      <h3>Yelpdemo is the best way to find reviews on local restaurants.</h3>
+      <p>
+        Explore the favorite diners pastry shops and eateries in your community.
+        <br />Leave reviews and ratings for your fellow foodies.
+      </p>
+    </div>
+    <Table>
+      <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+        <TableRow>
+          <TableHeaderColumn>Name</TableHeaderColumn>
+            <TableHeaderColumn>Address</TableHeaderColumn>
+            <TableHeaderColumn>Phone</TableHeaderColumn>
+            <TableHeaderColumn>Website</TableHeaderColumn>
+          </TableRow>
+      </TableHeader>
+      <TableBody displayRowCheckbox={false}>
+        {
+          restaurants.map(rest =>
+            <RestaurantListItem key={rest.id} restaurant={rest} />
+          )
+        }
+      </TableBody>
+    </Table>
+    <div className="img">
+      <img src={city}/>
+    </div>
+  </div>
 );
 
 const mapStateToProps = (state) => ({
