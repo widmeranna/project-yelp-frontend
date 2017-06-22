3538e3074da8d43f760c2553df26b4cedd190aca
widmeranna <widmeranna@gmail.com>
2017-06-22 09:27:23 +0200

    added description box and styling

--- src/components/RestaurantList/index.js
+++ src/components/RestaurantList/index.js
@@ -15,8 +15,8 @@ class RestaurantList extends Component {
     return(
       <div className="Home-List">
         <div className="descr-restaurant-list">
-          <h2>Yelpdemo is the best way to find reviews on local restaurants.</h2>
-          <h3> Explore the favorite diners pastry shops and eateries in your community. <br></br>Leave reviews and ratings for your fellow foodies. </h3>
+          <h3>Yelpdemo is the best way to find reviews on local restaurants.</h3>
+          <p> Explore the favorite diners pastry shops and eateries in your community. <br></br>Leave reviews and ratings for your fellow foodies. </p>
         </div>
         <Table>
           <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
