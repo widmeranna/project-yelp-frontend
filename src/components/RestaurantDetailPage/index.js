858081d43351154520399673464b4a18ac4553f9
Satuk Kolan <34@satuk.ch>
2017-06-23 08:35:23 +0200

    implemented rating component

--- src/components/RestaurantDetailPage/index.js
+++ src/components/RestaurantDetailPage/index.js
@@ -3,21 +3,31 @@ import {connect} from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
 import './index.css';
 import Reviews from '../Reviews';
+import { Rating } from 'material-ui-rating';
+import { calcAverageRating } from '../../utils';
+import { Link } from 'react-router-dom';
 
 class RestaurantDetailPage extends Component {
 	render(){
 		const {restaurant} = this.props;
+		const {reviews} = this.props;
+		const reviewsSize = Object.keys(reviews).length;
 
 		return(
 			<div className="restaurantDetailPage">
 				<div className="info">
 					<img src={restaurant.img} alt={restaurant.name} height="150"/>
 					<p>{restaurant.name}</p>
-					<p>* * * * *</p>
-					<p>689 reviews</p>
+					<Rating
+	          value={calcAverageRating(Object.values(reviews))}
+	          max={5}
+	          // onChange={(value) => console.log(`Rated with value ${value}`)}
+						readOnly={true}
+	        />
+					<p>{reviewsSize} reviews</p>
 					<p>{restaurant.address}</p>
 					<p>{restaurant.phone}</p>
-					<p>{restaurant.website}</p>
+					<p><Link to={'http://' + restaurant.website}>{restaurant.website}</Link></p>
 					<RaisedButton label="Write a Review"></RaisedButton>
 					<p>{restaurant.map}</p>
 				</div>
@@ -34,6 +44,7 @@ const mapStateToProps = (state, props) => {
 
 	return {
 		restaurant: state.restaurants[id],
+		reviews: state.reviews,
 	};
 };
 export default connect(mapStateToProps)(RestaurantDetailPage);
