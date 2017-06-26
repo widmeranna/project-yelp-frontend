ef1a8e7a35f3556b8868bee78084a7be56482007
Satuk Kolan <34@satuk.ch>
2017-06-26 10:15:12 +0200

    implemented the first view and dispatch for reviews

--- src/containers/NewReview/index.js
+++ src/containers/NewReview/index.js
@@ -1,47 +1,80 @@
 import React, {Component} from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
-import { Rating } from 'material-ui-rating';
+import {Rating} from 'material-ui-rating';
 import TextField from 'material-ui/TextField';
-
+import {grey50} from 'material-ui/styles/colors';
+import {connect} from 'react-redux';
+import {addReview} from '../../store/actions';
 
 class NewReview extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 
 		this.state = {
 			value: 0,
+			text: ''
 		}
 	}
 
-	render(){
-		return(
+	handleSubmit = (e) => {
+		e.preventDefault();
+
+		this.props.addReview({
+			value: this.state.value,
+			text: this.state.text
+		});
+
+		this.state = ({
+			value: 0,
+			text: '',
+		});
+	};
+
+	render() {
+		console.log('state:', this.state);
+		console.log('props:', this.props);
+
+		return (
 			<div>
 				<h1>New Review</h1>
-				<Rating
-					value={this.state.value}
-					max={5}
-					readOnly={false}
-					onChange={(value) => this.setState({value})}
+				<form onSubmit={this.handleSubmit}>
+					<Rating
+						value={this.state.value}
+						max={5}
+						readOnly={false}
+						onChange={(value) => this.setState({value})}
+					/>
+					<TextField
+						hintText="Comment"
+						floatingLabelText="Your Comment..."
+						multiLine={true}
+						fullWidth={true}
+						rows={8}
+						value={this.state.text}
+						onChange={(e, text) => this.setState({text})}
+					/>
+					<br/>
+					<RaisedButton
+						label="Create a Review"
+						backgroundColor="red"
+						labelColor={grey50}
+						type="submit"
+					/>
+					<br/>
+				</form>
+
+				<FlatButton
+					label="back"
+					onClick={this.props.back}
 				/>
-				<TextField
-		      hintText="MultiLine with rows: 2 and rowsMax: 4"
-		      multiLine={true}
-					fullWidth={true}
-		      rows={8}
-		      // rowsMax={4}
-		    />
-				<br />
-				<RaisedButton
-					label="Create a Review"
-					backgroundColor="red"
-					labelColor="white"
-				/>
-				<br />
-				<FlatButton label="back" onClick={this.props.back}/>
 			</div>
 		);
 	}
 }
 
-export default NewReview;
+const mapDispatchToProps = (dispatch) => ({
+	addReview: (review) => dispatch(addReview(review)),
+});
+
+export default connect(null, mapDispatchToProps)(NewReview);
